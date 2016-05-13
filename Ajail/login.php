c37674877b4364fa3b2026a3948c01d716af7a2c
shiibashi <shiibashitech2@gmail.com>
2016-05-13 15:06:28 +0900

    test byS

--- Ajail/login.php
+++ Ajail/login.php
@@ -37,6 +37,9 @@ $hs = new tohash();
 
 //セッションにはユーザーID入れておく
 $_SESSION["USERID"] = $_POST["userid"];
+$_SESSION["NAME"] = $_POST["family_name"];
+$_SESSION["FIRSTNAME"] = $_POST["first_name"];
+
 
 //入力IDからデータベース参照
 $table = "regist";
