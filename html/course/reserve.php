14b3257a40e271f1ae58459c2c10a12a84f19700
vim-general <hakuhakushinshiro@gmail.com>
2016-05-18 09:38:13 +0900

    reserve

--- html/course/reserve.php
+++ html/course/reserve.php
@@ -17,13 +17,18 @@ $col    = '';
 $data = $dbh->select($table,$col);
 //////////////////////////////////////////////////////////
 
+//jsでcookieに格納したarrayがphpだとstringになってしまっているので
+//cookieの中身をidからカナに変換してsessionに渡す
 $strId = $_COOKIE["cart"];
 $deleteWord = array('[', ']', '"');
+//邪魔な文字を削除する
 $strIdClean = str_replace($deleteWord, "", $strId);
+//カンマ起点に配列化
 $arrId2 = explode(",", $strIdClean);
 $arrId3 = array();
 $arrId4 = array();
 
+//cookieにあるidの商品のみ抽出
 foreach($data as $arr){
     foreach($arr as $key => $val){
         if($key === "id" && in_array($val, $arrId2)){
@@ -31,7 +36,6 @@ foreach($data as $arr){
         }
     }
 };
-
 foreach($arrId3 as $arr1){
     foreach($arr1 as $key1 => $val1){
         if($key1 === "name"){
@@ -39,12 +43,8 @@ foreach($arrId3 as $arr1){
         }
     }
 };
-
 $_SESSION["dish"] = $arrId4;
-
-echo "セッションの中に";
-print_r($_SESSION["dish"]);
-echo "が入りましたよ";
+////////////////////////////////////////////////
 
 include_once('./reserve.html');
 
